f6584603a4e4260bee0f8399b4a4f7d31d38b60c
juanchehin <chehinjuan@gmail.com>
2025-11-03 12:05:03 -0300

    fix create database

--- database/migrations/2023_07_24_015918_create_permission_tables.php
+++ database/migrations/2023_07_24_015918_create_permission_tables.php
@@ -26,8 +26,8 @@ class CreatePermissionTables extends Migration
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('guard_name');
+            $table->string('name', 125);
+            $table->string('guard_name', 125);
             $table->timestamps();
 
             $table->unique(['name', 'guard_name']);
@@ -39,8 +39,8 @@ class CreatePermissionTables extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
                 $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
             }
-            $table->string('name');
-            $table->string('guard_name');
+            $table->string('name', 125);
+            $table->string('guard_name',125);
             $table->timestamps();
 
             if ($teams || config('permission.testing')) {
@@ -53,7 +53,7 @@ class CreatePermissionTables extends Migration
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
             $table->unsignedBigInteger('permission_id');
 
-            $table->string('model_type');
+            $table->string('model_type',125);
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
@@ -81,7 +81,7 @@ class CreatePermissionTables extends Migration
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
             $table->unsignedBigInteger('role_id');
 
-            $table->string('model_type');
+            $table->string('model_type', 125);
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
 
