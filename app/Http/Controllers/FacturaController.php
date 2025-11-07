5b404fd3dfccdb412400d2fd466a84ff9d51a623
juanchehin <chehinjuan@gmail.com>
2025-11-07 12:41:52 -0300

    fact controller

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -6,6 +6,22 @@ use Illuminate\Http\Request;
 
 class FacturaController extends Controller
 {
+    public function index()
+    {
+        // Mostrar listado de facturas
+        return view('facturas.index');
+    }
+
+    public function create()
+    {
+        // Mostrar formulario para crear una nueva factura
+        return view('facturas.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Guardar una nueva factura
+    }
     //
     public function pendientes()
     {
