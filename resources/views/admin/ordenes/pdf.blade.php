8d6cbce29b6a27feb5bc46bc0508b1108d44a436
juanchehin <chehinjuan@gmail.com>
2026-02-19 19:44:28 -0300

    nombre de la unidad en pdf

--- resources/views/admin/ordenes/pdf.blade.php
+++ resources/views/admin/ordenes/pdf.blade.php
@@ -68,6 +68,56 @@
     $fecha_entrega = $orden->fecha_entrega ? \Carbon\Carbon::parse($orden->fecha_entrega)->format('d/m/Y') : '-';
 @endphp
 
+@php
+    $unidades = [
+        1 => 'kilogramos',
+        2 => 'metros',
+        3 => 'metros cuadrados',
+        4 => 'metros cúbicos',
+        5 => 'litros',
+        6 => '1000 kWh',
+        7 => 'unidades',
+        8 => 'pares',
+        9 => 'docenas',
+        10 => 'quilates',
+        11 => 'millares',
+        14 => 'gramos',
+        15 => 'milímetros',
+        16 => 'mm cúbicos',
+        17 => 'kilómetros',
+        18 => 'hectolitros',
+        20 => 'centímetros',
+        25 => 'jgo. pqt. mazo naipes',
+        27 => 'cm cúbicos',
+        29 => 'toneladas',
+        30 => 'dam cúbicos',
+        31 => 'hm cúbicos',
+        32 => 'km cúbicos',
+        33 => 'microgramos',
+        34 => 'nanogramos',
+        35 => 'picogramos',
+        41 => 'miligramos',
+        47 => 'mililitros',
+        48 => 'curie',
+        49 => 'milicurie',
+        50 => 'microcurie',
+        51 => 'uiacthor',
+        52 => 'muiacthor',
+        53 => 'kg base',
+        54 => 'gruesa',
+        61 => 'kg bruto',
+        62 => 'uiactant',
+        63 => 'muiactant',
+        64 => 'uiactig',
+        65 => 'muiactig',
+        66 => 'kg activo',
+        67 => 'gramo activo',
+        68 => 'gramo base',
+        96 => 'packs',
+        98 => 'otras unidades',
+    ];
+@endphp
+
 
 {{-- ENCABEZADO --}}
 <table style="width:100%; margin-bottom:6px;">
@@ -189,7 +239,11 @@
             <td>{{ $item->codigo ?: '-' }}</td>
             <td>{{ $item->descripcion ?: '-' }}</td>
             <td>{{ $item->fecha_entrega ? \Carbon\Carbon::parse($item->fecha_entrega)->format('d/m/Y') : '-' }}</td>
-            <td>{{ $item->unidad ?: '-' }}</td>
+            <td>
+                {{ $item->unidad && isset($unidades[$item->unidad])
+                    ? $unidades[$item->unidad]
+                    : '-' }}
+            </td>
             <td>{{ number_format($item->cantidad, 2, ',', '.') }}</td>
             <td>{{ number_format($item->precio_unitario, 2, ',', '.') }}</td>
             <td>{{ number_format($item->iva ?? 0, 2, ',', '.') }}</td>
